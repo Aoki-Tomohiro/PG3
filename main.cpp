e6c0c56771cb40850efb38b07d0cec3ff7e8a1a4
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2023-10-16 11:02:44 +0900

    01_04_確認課題

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
-#include <stdio.h>
+#include <iostream>
+#include <functional>
+#include <random>
+#include <string>
+#include <Windows.h>
+
+void Game(std::function<int(std::mt19937&)> func, std::mt19937& randomEngine) {
+	std::cout << "奇数か偶数を選んでください" << "\n" << "1:奇数" << " " << "2:偶数" << std::endl;
+	std::string num;
+	std::cin >> num;
+	Sleep(3 * 1000);
+	int result = func(randomEngine);
+
+	if (result % 2 == 0) {
+		if (num == "1") {
+			std::cout << "サイコロの出目" << " " << result << "\n" << "GameOver" << std::endl;
+		}
+		else if (num == "2") {
+			std::cout << "サイコロの出目" << " " << result << "\n" << "GameClear" << std::endl;
+		}
+	}
+	else if (result % 2 == 1) {
+		if (num == "1") {
+			std::cout << "サイコロの出目" << " " << result << "\n" << "GameClear" << std::endl;
+		}
+		else if (num == "2") {
+			std::cout << "サイコロの出目" << " " << result << "\n" << "GameOver" << std::endl;
+		}
+	}
+}
 
 int main() {
+
+	//ランダム関数の初期化
+	std::random_device seedGenerator;
+	std::mt19937 randomEngine(seedGenerator());
+
+	std::function<int(std::mt19937&)> dice =
+		[&](std::mt19937& randomEngine) {
+		std::uniform_int_distribution<> distribution(1, 6);
+		return distribution(randomEngine);
+	};
+
+	Game(dice, randomEngine);
 
 	return 0;
 }

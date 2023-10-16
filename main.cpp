1ce10f11da61444ceb767e69b87183ba5ba4eb7d
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2023-10-16 10:42:06 +0900

    01_03_確認課題

--- main.cpp
+++ main.cpp
@@ -3,34 +3,34 @@
 #include <string>
 #include <Windows.h>
 
-typedef int (*pRandom)(std::mt19937&);
+typedef int (*pFunc)(std::mt19937&);
 
 int Dice(std::mt19937& randomEngine) {
 	std::uniform_int_distribution<> distribution(1, 6);
 	return distribution(randomEngine);
 }
 
-void Game(pRandom func, int second, std::mt19937& randomEngine) {
-
-	std::cout << "奇数 : 1  偶数 : 2" << std::endl;
+void Game(pFunc p, int second, std::mt19937& randomEngine) {
+	std::cout << "奇数か偶数を選んでください" << "\n" << "1:奇数" << " " << "2:偶数" << std::endl;
 	std::string num;
 	std::cin >> num;
 	Sleep(second * 1000);
-	int result = func(randomEngine);
+	int result = p(randomEngine);
+
 	if (result % 2 == 0) {
 		if (num == "1") {
-			std::cout << result << " " << "GameOver" << std::endl;
+			std::cout  << "サイコロの出目" << " " << result << "\n" << "GameOver" << std::endl;
 		}
 		else if (num == "2") {
-			std::cout << result << " " << "GameClear" << std::endl;
+			std::cout << "サイコロの出目" << " " << result << "\n" << "GameClear" << std::endl;
 		}
 	}
 	else if(result % 2 == 1) {
 		if (num == "1") {
-			std::cout << result << " " << "GameClear" << std::endl;
+			std::cout << "サイコロの出目" << " " << result << "\n" << "GameClear" << std::endl;
 		}
 		else if (num == "2") {
-			std::cout << result << " " << "GameOver" << std::endl;
+			std::cout << "サイコロの出目" << " " << result << "\n" << "GameOver" << std::endl;
 		}
 	}
 }

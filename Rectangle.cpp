ee013bb83ab98f1f31ac67808890d1507042360f
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2023-11-05 16:46:33 +0900

    02_04_確認課題

---
+++ Rectangle.cpp
@@ -0,0 +1,19 @@
+﻿#include "Rectangle.h"
+#include <iostream>
+
+Rectangle::Rectangle(float width, float height) {
+	width_ = width;
+	height_ = height;
+}
+
+Rectangle::~Rectangle() {
+
+}
+
+void Rectangle::size() {
+	area_ = width_ * height_;
+}
+
+void Rectangle::draw() {
+	std::cout << "矩形の面積は" << area_ << "です" << std::endl;
+}

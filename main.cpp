66728e122c077c065338bee238a101d5aaff89d7
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2023-11-13 16:48:53 +0900

    01_04_確認課題

--- main.cpp
+++ main.cpp
@@ -4,44 +4,57 @@
 #include <string>
 #include <Windows.h>
 
-void Game(std::function<int(std::mt19937&)> func, std::mt19937& randomEngine) {
-	std::cout << "奇数か偶数を選んでください" << "\n" << std::endl;
-	std::string num;
-	std::cin >> num;
-	Sleep(3 * 1000);
-	int result = func(randomEngine);
+typedef void (*pFunc)(std::string&, int);
 
+int DiceRoll(std::mt19937& randomEngine) {
+	std::uniform_int_distribution<> distribution(1, 6);
+	return distribution(randomEngine);
+}
+
+void DispResult(std::string& answer, int result) {
 	if (result % 2 == 0) {
-		if (num == "奇数") {
-			std::cout << "サイコロの出目" << " " << result << "\n" << "GameOver" << std::endl;
+		if (answer == "奇数") {
+			std::cout << "サイコロの出目:" << result << " " << "不正解" << std::endl;
 		}
-		else if (num == "偶数") {
-			std::cout << "サイコロの出目" << " " << result << "\n" << "GameClear" << std::endl;
+		else if (answer == "偶数") {
+			std::cout << "サイコロの出目:" << result << " " << "正解" << std::endl;
 		}
 	}
 	else if (result % 2 == 1) {
-		if (num == "奇数") {
-			std::cout << "サイコロの出目" << " " << result << "\n" << "GameClear" << std::endl;
+		if (answer == "奇数") {
+			std::cout << "サイコロの出目:" << result << " " << "正解" << std::endl;
 		}
-		else if (num == "偶数") {
-			std::cout << "サイコロの出目" << " " << result << "\n" << "GameOver" << std::endl;
+		else if (answer == "偶数") {
+			std::cout << "サイコロの出目:" << result << " " << "不正解" << std::endl;
 		}
 	}
 }
 
-int main() {
+void SetTimeout(pFunc p, int second, std::string& answer, int result) {
+	Sleep(second * 1000);
+	p(answer, result);
+}
 
+int main() {
 	//ランダム関数の初期化
 	std::random_device seedGenerator;
 	std::mt19937 randomEngine(seedGenerator());
 
-	std::function<int(std::mt19937&)> dice =
-		[&](std::mt19937& randomEngine) {
-		std::uniform_int_distribution<> distribution(1, 6);
-		return distribution(randomEngine);
-	};
+	//止める秒数
+	int second = 3;
 
-	Game(dice, randomEngine);
+	//std::function
+	std::function<void(pFunc p, int second, std::string&, int)> fx = SetTimeout;
+
+	//ラムダ式
+	[fx,second,&randomEngine]() {
+		std::cout << "奇数か偶数を選んでください" << std::endl;
+		std::string str;
+		std::cin >> str;
+		void (*pFunc)(std::string&, int) = &DispResult;
+		int result = DiceRoll(randomEngine);
+		fx(pFunc, second, str, result);
+	}();
 
 	return 0;
 }

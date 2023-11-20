53f7c9ec57ad0e125358b4f02b1777e6448772c0
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2023-11-20 11:57:15 +0900

    01_04_確認課題

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@
 #include <string>
 #include <Windows.h>
 
-typedef void (*pFunc)(std::string&, int);
+typedef void (*pFunc)(std::string&);
 
-int DiceRoll(std::mt19937& randomEngine) {
+int DiceRoll() {
+	std::random_device seedGenerator;
+	std::mt19937 randomEngine(seedGenerator());
 	std::uniform_int_distribution<> distribution(1, 6);
 	return distribution(randomEngine);
 }
 
-void DispResult(std::string& answer, int result) {
+void DispResult(std::string& answer) {
+	int result = DiceRoll();
 	if (result % 2 == 0) {
 		if (answer == "奇数") {
 			std::cout << "サイコロの出目:" << result << " " << "不正解" << std::endl;
@@ -30,30 +33,20 @@ void DispResult(std::string& answer, int result) {
 	}
 }
 
-void SetTimeout(pFunc p, int second, std::string& answer, int result) {
+void SetTimeout(pFunc p, int second, std::string& answer) {
 	Sleep(second * 1000);
-	p(answer, result);
+	p(answer);
 }
 
 int main() {
-	//ランダム関数の初期化
-	std::random_device seedGenerator;
-	std::mt19937 randomEngine(seedGenerator());
-
-	//止める秒数
-	int second = 3;
-
-	//std::function
-	std::function<void(pFunc p, int second, std::string& answer, int result)> fx = SetTimeout;
-
-	//ラムダ式
-	[fx,second,&randomEngine]() {
+	std::function<void()> fx = []() {
 		std::cout << "奇数か偶数を選んでください" << std::endl;
-		std::string str;
-		std::cin >> str;
-		int result = DiceRoll(randomEngine);
-		fx(DispResult, second, str, result);
-	}();
+		std::string answer;
+		std::cin >> answer;
+		SetTimeout(DispResult, 3, answer);
+	};
+
+	fx();
 
 	return 0;
 }

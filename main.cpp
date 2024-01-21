3423269c82a33c3bdea8cb95db487444e66c31b8
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2024-01-22 02:26:08 +0900

    06_02_確認課題

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <stdio.h>
+#include <iostream>
+#include <thread>
 
-int main() {
+void PrintString(const std::string& str)
+{
+	std::cout << str << std::endl;
+}
+
+int main() 
+{
+    std::thread th1(PrintString, "thread 1");
+    th1.join();
+    std::thread th2(PrintString, "thread 2");
+    th2.join();
+    std::thread th3(PrintString, "thread 3");
+    th3.join();
 
 	return 0;
 }
